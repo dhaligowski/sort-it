0113cb767b7bb3b42b2ab5e7dc22add77bd4b01f
DHaligowski <dhaligowski@gmail.com>
2021-03-25 12:04:12 -0400

    Refactor selection sort render counter.

--- src/sortingAlgorithms/selectionSortAnimations.jsx
+++ src/sortingAlgorithms/selectionSortAnimations.jsx
@@ -3,29 +3,21 @@ import { swap } from "../animations/animationVisuals";
 import { animateSwap } from "../animations/animationVisuals";
 import { animateCompareEnd } from "../animations/animationVisuals";
 
-const selectionSortAnimations = (array) => {
-  let renderCounter = 0;
+const selectionSortAnimations = (array, renderCounter) => {
   for (let i = 0; i < array.length; i++) {
     let minIndex = i;
     for (let j = i; j < array.length; j++) {
-      renderCounter++;
       animateCompare(i, j, renderCounter);
       if (array[j] < array[minIndex]) {
-        renderCounter++;
         animateCompareEnd(minIndex, minIndex, renderCounter);
         minIndex = j;
       }
       if (j !== minIndex) {
-        renderCounter++;
         animateCompareEnd(i, j, renderCounter);
       }
     }
-
     swap(array, minIndex, i, renderCounter);
-    renderCounter++;
     animateSwap(minIndex, i, renderCounter);
-
-    renderCounter++;
     animateCompareEnd(minIndex, i, renderCounter);
   }
   return array;
