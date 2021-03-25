2b067d61b34d6bd4a6b0dd9bef3cd022ec7f32be
DHaligowski <dhaligowski@gmail.com>
2021-03-25 12:03:54 -0400

    Refactor insertion sort render counter.

--- src/sortingAlgorithms/insertionSortAnimations.jsx
+++ src/sortingAlgorithms/insertionSortAnimations.jsx
@@ -3,26 +3,19 @@ import { animateShift } from "../animations/animationVisuals";
 import { animateShiftCurrent } from "../animations/animationVisuals";
 import { animateCompareEnd } from "../animations/animationVisuals";
 
-const insertionSortAnimations = (array) => {
-  let renderCounter = 0;
+const insertionSortAnimations = (array, renderCounter) => {
   for (let i = 1; i < array.length; i++) {
     let current = array[i];
     let j = i - 1;
     while (j >= 0 && array[j] > current) {
-      renderCounter++;
       animateCompare(j, j + 1, renderCounter);
-
       array[j + 1] = array[j]; //shift  greater value
-      renderCounter++;
       animateShift(j, j + 1, renderCounter);
-      renderCounter++;
       animateCompareEnd(j, j + 1, renderCounter);
       j = j - 1; //decrement j to see if < or at zero
     }
-    renderCounter++; //
     animateCompare(j + 1, j + 1, renderCounter); //
     array[j + 1] = current; //save current in new home
-    renderCounter++;
     animateShiftCurrent(j + 1, current, renderCounter);
   }
   return array;
