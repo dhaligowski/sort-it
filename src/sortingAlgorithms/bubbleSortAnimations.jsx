c793bc219e18ed94ee15b0ce2a15ad8ef4a9b1bb
DHaligowski <dhaligowski@gmail.com>
2021-03-25 11:32:55 -0400

    Refactor bubble sort render counter.

--- src/sortingAlgorithms/bubbleSortAnimations.jsx
+++ src/sortingAlgorithms/bubbleSortAnimations.jsx
@@ -3,26 +3,30 @@ import { swap } from "../animations/animationVisuals";
 import { animateSwap } from "../animations/animationVisuals";
 import { animateCompareEnd } from "../animations/animationVisuals";
 
-const bubbleSortAnimations = (array) => {
-  let renderCounter = 0; //counter to make animation smooth
+const bubbleSortAnimations = (array, renderCounter) => {
+  //let renderCounter = 0; //counter to make animation smooth
   for (var i = 0; i < array.length; i++) {
     let isSorted = true;
     for (var j = 1; j < array.length - i; j++)
       if (array[j] < array[j - 1]) {
-        renderCounter++;
+        //renderCounter++;
+        console.log("RC", renderCounter);
         animateCompare(j - 1, j, renderCounter);
-        renderCounter++;
+        //renderCounter++;
+        console.log("RC", renderCounter);
         swap(array, j - 1, j, renderCounter);
         animateSwap(j - 1, j, renderCounter);
+        console.log("RC", renderCounter);
 
-        renderCounter++;
+        //renderCounter++;
         animateCompareEnd(j - 1, j, renderCounter);
+        console.log("RC", renderCounter);
 
         isSorted = false;
       } else {
-        renderCounter++;
+        //renderCounter++;
         animateCompare(j - 1, j, renderCounter);
-        renderCounter++;
+        // renderCounter++;
         animateCompareEnd(j - 1, j, renderCounter);
       }
 
