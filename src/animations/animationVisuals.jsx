5f675e748ad13fc0b68440539853a69b841911ed
DHaligowski <dhaligowski@gmail.com>
2021-03-24 23:57:51 -0400

    Refactor animation helper functions.

---
+++ src/animations/animationVisuals.jsx
@@ -0,0 +1,93 @@
+import colors from "../config/colors";
+import constants from "../config/constants";
+
+export function animateCompare(index1, index2, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  setTimeout(() => {
+    arrayBars[index1].style.backgroundColor = colors.secondary;
+    arrayBars[index2].style.backgroundColor = colors.secondary;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function animateCompareEnd(index1, index2, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  setTimeout(() => {
+    arrayBars[index1].style.backgroundColor = colors.primary;
+    arrayBars[index2].style.backgroundColor = colors.primary;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function swap(array, index1, index2, i) {
+  var temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+}
+
+export function animateSwap(index1, index2, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  const arrayValue = document.getElementsByClassName("array-value");
+
+  setTimeout(() => {
+    const tempArrayValue = arrayValue[index1].innerHTML;
+    arrayValue[index1].innerHTML = arrayValue[index2].innerHTML;
+    arrayValue[index2].innerHTML = tempArrayValue;
+
+    const tempStyle = arrayBars[index1].style.height;
+    arrayBars[index1].style.height = arrayBars[index2].style.height;
+    arrayBars[index2].style.height = tempStyle;
+
+    arrayBars[index1].style.backgroundColor = colors.primary;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function animateShiftCurrent(index1, current, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  const arrayValue = document.getElementsByClassName("array-value");
+
+  setTimeout(() => {
+    arrayValue[index1].innerHTML = current;
+    arrayBars[index1].style.height = `${current}px`;
+    arrayBars[index1].style.backgroundColor = colors.primary;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function animateShift(index1, index2, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  const arrayValue = document.getElementsByClassName("array-value");
+  setTimeout(() => {
+    arrayValue[index2].innerHTML = arrayValue[index1].innerHTML;
+    arrayBars[index2].style.height = arrayBars[index1].style.height;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function animatePivot(index1, index2, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  setTimeout(() => {
+    arrayBars[index1].style.backgroundColor = colors.pivot;
+    arrayBars[index2].style.backgroundColor = colors.pivot;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function animateMerge(index1, value, renderCounter) {
+  console.log("animateValue", index1, value, renderCounter);
+  const arrayBars = document.getElementsByClassName("array-bar");
+  const arrayValue = document.getElementsByClassName("array-value");
+  setTimeout(() => {
+    arrayValue[index1].innerHTML = value;
+    arrayBars[index1].style.height = `${value}px`;
+  }, renderCounter * constants.ANIMATION_SPEED_MS);
+}
+
+export function animateMergeCompare(index1, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  setTimeout(() => {
+    arrayBars[index1].style.backgroundColor = colors.secondary;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
+
+export function animateMergeCompareEnd(index1, i) {
+  const arrayBars = document.getElementsByClassName("array-bar");
+  setTimeout(() => {
+    arrayBars[index1].style.backgroundColor = colors.primary;
+  }, i * constants.ANIMATION_SPEED_MS);
+}
