532a0fb18363467337cbd821301af03a392aa71e
DHaligowski <dhaligowski@gmail.com>
2021-03-30 15:07:02 -0400

    Increase animation speed.

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  ANIMATION_SPEED_MS: 20, //changes the speed of the animation
+  ANIMATION_SPEED_MS: 10, //changes the speed of the animation in MS
   NUMBER_OF_ARRAY_BARS: 31, //changes the number of bars generated
   OVERFLOW_BARS: 1, //one bar will overflow into a second row upon window resize
   RANGE_BEGIN: 1, //beginning range for array bars
